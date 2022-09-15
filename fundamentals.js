6c20c388f56ae72a217867f7cbfa9c22f6ad1b1d
Stephen Power <stephenxpower2003@gmail.com>
2022-09-15 18:09:22 +0100

    Arrays.

--- fundamentals.js
+++ fundamentals.js
@@ -29,4 +29,7 @@ aCar.colour = {
 console.log(aCar.owner + ' drives a ' + aCar.type.make)
 console.log('The registration number is: ' + aCar.registration.year + '-' + aCar.registration.code + '-' + aCar.registration.number)
 console.log('It is a ' + aCar.colour.exterior + ' car, ' + aCar.mileage + ' mileage, with ' + aCar.colour.interior.texture + ' interior')
-console.log('First owner : ' + aCar.previous_owners[0].name + ', ' + aCar.previous_owners[0].address)
+console.log('Previous Owners:')
+for (let i = 0 ; i < aCar.previous_owners.length ; i += 1) {
+  console.log(aCar.previous_owners[i].name + ', ' + aCar.previous_owners[i].address);
+}
