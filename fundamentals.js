7b1fee1a94cf13de7b207626611f4f4bea64492d
Stephen Power <stephenxpower2003@gmail.com>
2022-09-15 17:45:11 +0100

    Dynamic Properties.

--- fundamentals.js
+++ fundamentals.js
@@ -11,6 +11,17 @@ const aCar = {
       number: 1058
     },
   };
-  
+
+aCar.mileage = 10000 ;
+aCar.colour = {
+  exterior : 'red',
+  interior : {
+    texture : 'leather',
+    shade: 'cream'
+  }
+
+}
+
 console.log(aCar.owner + ' drives a ' + aCar.type.make)
 console.log('The registration number is: ' + aCar.registration.year + '-' + aCar.registration.code + '-' + aCar.registration.number)
+console.log('It is a ' + aCar.colour.exterior + ' car, ' + aCar.mileage + ' mileage, with ' + aCar.colour.interior.texture + ' interior')
