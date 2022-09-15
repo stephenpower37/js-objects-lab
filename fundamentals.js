22931723075c8389959933c21f76b41a0098c772
Stephen Power <stephenxpower2003@gmail.com>
2022-09-15 18:04:23 +0100

    Arrays.

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,9 @@
 const aCar = {
     owner : "Joe Bloggs",
+    previous_owners: [
+      { name : 'Pat Smith', address: '1 Main Street' },
+      { name : 'Sheila Dwyer', address: '2 High Street' }
+    ],
     type : {
       make: 'Toyota',
       model: 'Corolla',
@@ -25,3 +29,4 @@ aCar.colour = {
 console.log(aCar.owner + ' drives a ' + aCar.type.make)
 console.log('The registration number is: ' + aCar.registration.year + '-' + aCar.registration.code + '-' + aCar.registration.number)
 console.log('It is a ' + aCar.colour.exterior + ' car, ' + aCar.mileage + ' mileage, with ' + aCar.colour.interior.texture + ' interior')
+console.log('First owner : ' + aCar.previous_owners[0].name + ', ' + aCar.previous_owners[0].address)
