46862e82f8bb031081d71cb97aa0357a1b9bc7f9
Stephen Power <stephenxpower2003@gmail.com>
2022-09-15 18:11:27 +0100

    Looping.

--- fundamentals.js
+++ fundamentals.js
@@ -29,7 +29,7 @@ aCar.colour = {
 console.log(aCar.owner + ' drives a ' + aCar.type.make)
 console.log('The registration number is: ' + aCar.registration.year + '-' + aCar.registration.code + '-' + aCar.registration.number)
 console.log('It is a ' + aCar.colour.exterior + ' car, ' + aCar.mileage + ' mileage, with ' + aCar.colour.interior.texture + ' interior')
-console.log('Previous Owners:')
+console.log('Previous Owners: ')
 for (let i = 0 ; i < aCar.previous_owners.length ; i += 1) {
   console.log(aCar.previous_owners[i].name + ', ' + aCar.previous_owners[i].address);
 }
